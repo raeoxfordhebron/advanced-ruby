404f919ce97dd38f8f01e589d01f508db48782f4
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-04-18 20:23:00 -0400

    intial commit

---
+++ test.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+# Regular Expressions
+line1 = "Cats are smarter than dogs"
+line2 = "Dogs also like meat"
+
+if( line1 =~ /Cats(.*)/)
+    puts "Line1 contains Cats"
+end
+if( line2 =~ /Cats(.*)/)
+    puts "Line2 contains Dogs"
+end
